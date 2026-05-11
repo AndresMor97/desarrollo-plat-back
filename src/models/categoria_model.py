78f7641010e1893f4f0e4e4ad8e9116d93f2645a
Andres <edwin.morales-s@uniminuto.edu.co>
2026-05-10 22:42:32 -0500

    Se modifica categoria_model para asociar una categoria a un usuario

--- src/models/categoria_model.py
+++ src/models/categoria_model.py
@@ -1,10 +1,11 @@
 from src.models.transaccion_model import db
 
-
 class Categoria(db.Model):
     __tablename__ = 'categorias'
 
     id_categoria = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # Nueva columna conectada al usuario. nullable=True permite las categorías por defecto
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'), nullable=True) 
     nombre = db.Column(db.String(50), nullable=False)
     tipo = db.Column(db.Enum('ingreso', 'gasto'), nullable=False)
 
