fd9bbe066361a3d15c00e79ae0e620c0cae4e6f3
Andres <edwin.morales-s@uniminuto.edu.co>
2026-05-10 23:40:18 -0500

    se actualizan rutas para categorias

--- src/routes/categoria_routes.py
+++ src/routes/categoria_routes.py
@@ -2,7 +2,8 @@ from flask import Blueprint, request
 from src.controllers.categoria_controller import (
     listar_categorias_logic, 
     crear_categoria_logic,
-    eliminar_categoria_logic # <-- Nueva importación
+    eliminar_categoria_logic,
+    obtener_saldos_por_categoria_logic # <-- Nueva importación
 )
 from src.utils.auth_middleware import token_required
 
@@ -20,8 +21,13 @@ def post_categoria(current_user):
     data = request.get_json()
     return crear_categoria_logic(data, current_user)
 
-# <-- NUEVO ENDPOINT PARA ELIMINAR -->
 @categoria_bp.route('/categorias/<int:id_categoria>', methods=['DELETE'])
 @token_required
 def delete_categoria(current_user, id_categoria):
     return eliminar_categoria_logic(id_categoria, current_user)
+
+# <-- NUEVO ENDPOINT PARA SALDOS POR CATEGORÍA -->
+@categoria_bp.route('/categorias/saldos', methods=['GET'])
+@token_required
+def get_saldos_categorias(current_user):
+    return obtener_saldos_por_categoria_logic(current_user)
