f68f77aedae9db9403f0911589db596e0b3fca07
Andres <edwin.morales-s@uniminuto.edu.co>
2026-05-10 22:45:52 -0500

    Se agrega metodo POST para agregar nuevas categorias

--- src/routes/categoria_routes.py
+++ src/routes/categoria_routes.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, request
-from src.controllers.categoria_controller import listar_categorias_logic
+from src.controllers.categoria_controller import listar_categorias_logic, crear_categoria_logic
+from src.utils.auth_middleware import token_required
 
 categoria_bp = Blueprint('categoria_bp', __name__)
 
-
 @categoria_bp.route('/categorias', methods=['GET'])
-def get_categorias():
-    """Devuelve una lista de categorías con filtro opcional por tipo."""
+@token_required # <-- Ahora exige token
+def get_categorias(current_user):
     tipo = request.args.get('tipo')
-    return listar_categorias_logic(tipo)
+    return listar_categorias_logic(tipo, current_user)
+
+@categoria_bp.route('/categorias', methods=['POST'])
+@token_required # <-- Endpoint para crear
+def post_categoria(current_user):
+    data = request.get_json()
+    return crear_categoria_logic(data, current_user)
