1747cf6f1e08101e53f89b494414abbbc52d00ba
Andres <edwin.morales-s@uniminuto.edu.co>
2026-05-10 22:43:23 -0500

    se modifica dto de categoria

--- src/dtos/categoria_dto.py
+++ src/dtos/categoria_dto.py
@@ -1,11 +1,10 @@
 from flask_marshmallow import Marshmallow
-from marshmallow import fields
+from marshmallow import fields, validate
 
 ma = Marshmallow()
 
-
 class CategoriaDTO(ma.Schema):
-    """Schema para serializar y validar categorías."""
-    id_categoria = fields.Int()
-    nombre = fields.Str()
-    tipo = fields.Str()
+    id_categoria = fields.Int(dump_only=True)
+    id_usuario = fields.Int(dump_only=True) # Protegido, lo asignará el backend
+    nombre = fields.Str(required=True, validate=validate.Length(min=1, max=50))
+    tipo = fields.Str(required=True, validate=validate.OneOf(["ingreso", "gasto"]))
