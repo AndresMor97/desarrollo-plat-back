2bf6a04cd38def4d1b2319411952b308037acb09
Andres <edwin.morales-s@uniminuto.edu.co>
2026-05-10 23:34:50 -0500

    se agrega dto para manejar saldos por categoria

--- src/dtos/categoria_dto.py
+++ src/dtos/categoria_dto.py
@@ -8,3 +8,10 @@ class CategoriaDTO(ma.Schema):
     id_usuario = fields.Int(dump_only=True) # Protegido, lo asignará el backend
     nombre = fields.Str(required=True, validate=validate.Length(min=1, max=50))
     tipo = fields.Str(required=True, validate=validate.OneOf(["ingreso", "gasto"]))
+
+# NUEVO DTO: Para estructurar la respuesta del saldo acumulado por cada categoría
+class SaldoCategoriaDTO(ma.Schema):
+    id_categoria = fields.Int(dump_only=True)
+    nombre = fields.Str(dump_only=True)
+    tipo = fields.Str(dump_only=True)
+    total_acumulado = fields.Float(dump_only=True)
